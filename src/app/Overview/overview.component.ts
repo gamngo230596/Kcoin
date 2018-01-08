a33b9286ea9606f06ba88a64cfa25a4857d55085
Tai Chau <tai.chaule@gmail.com>
2018-01-09 01:24:42 +0700

    fix overview + recharge + transaction

--- src/app/Overview/overview.component.ts
+++ src/app/Overview/overview.component.ts
@@ -8,8 +8,14 @@ import { Router, ActivatedRoute, Params } from '@angular/router';
 })
 export class OverviewComponent {
     constructor(public nav: NavService,private route: Router){}
+    balance;
+    address;
+    UserCurr:any=[];
     ngOnInit(){
-    	
+    	this.nav.getUser().subscribe(user=>{this.UserCurr=user});
+    	setTimeout(()=>{console.log(this.UserCurr);},5000);
+    	//this.nav.getBalance().subscribe(balance=>{this.balance=balance});
+        //setTimeout(()=>{console.log(this.balance);},5000);
     }
     
 }

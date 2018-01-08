960fc4ec4c98065f15c6bcf997d752fb3f8b83b5
Tai Chau <tai.chaule@gmail.com>
2018-01-09 01:21:36 +0700

    send mail xác nhận chuyển tiền

--- src/app/Withdrawal/withdrawal.component.ts
+++ src/app/Withdrawal/withdrawal.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavService } from '../Service/nav.service';
+import { NavService,IMessage } from '../Service/nav.service';
 import { Router, ActivatedRoute, Params } from '@angular/router';
 @Component({
     selector: 'app-withdrawal',
@@ -8,35 +8,65 @@ import { Router, ActivatedRoute, Params } from '@angular/router';
 })
 export class WithdrawalComponent {
     constructor(public nav: NavService,private route: Router){
-    	this.status="Khởi tạo";
+        setTimeout(()=>{
+
+        },60000);
     }
     actual=0;
     available=0;
-    Address:any;
-    status="Khởi tạo";
+    status;
     messageErr;
+    message:IMessage={}
     transaction:any=[];
     ngOnInit(){
         console.log(this.nav.address);
+        if(this.nav.address!=="" && this.nav.id!=="")
+        {
     	this.nav.getBalance(this.nav.address).subscribe(balance=>{this.actual=balance;this.available=balance});
-       	setTimeout(()=>{console.log(this.actual);},4000);
-        this.nav.getTransaction(this.nav.address).subscribe(trans=>{this.transaction=trans});
-        setTimeout(()=>{console.log(this.transaction);},4000);
-        /*this.nav.updateActualBalance(this.nav.id,this.actual).subscribe(
+       	setTimeout(()=>{
+             this.nav.updateActualBalance(this.nav.id,this.actual).subscribe(
                 data=>console.log(data),
                 error=>console.log(error));
-        this.nav.updateAvailableBalance(this.nav.id,this.available).subscribe(
+            this.nav.updateAvailableBalance(this.nav.id,this.available).subscribe(
                 data=>console.log(data),
-                error=>console.log(error));*/
+                error=>console.log(error));
+        },4000);
+        this.nav.getTransaction(this.nav.address).subscribe(trans=>{this.transaction=trans});
+        setTimeout(()=>{console.log(this.transaction);},4000); 
+    }
         
     }
     Send(address,money){
-    	this.Address=address;
     	this.nav.transactionsHis(this.nav.address,address,this.status).subscribe(
             data=>console.log(data),
             error=>console.log(error),
             ()=>this.messageErr="Success!");
 
     }
+    VerifyTrans(code){
+        for(let i=0;i<this.transaction.length;i++)
+        {
+            if(this.transaction[i].address.toString()===code.toString()){
+                if(this.nav.email!=="")
+                {
+                    let obj={
+                        email: this.nav.email,
+                        id:this.transaction[i]._id
+                    }
+                    this.message=obj;
+                    /*setTimeout(()=>{
+                        this.nav.sendEmailStatus(this.message).subscribe(res=>{
+                            console.log('Success!',res);
+                        },error=>{
+                            console.log('Err',error);
+                        });
+                    },2000);*/
+                    
+                }
+                
+            }
+        }
+    }
+
      
 }

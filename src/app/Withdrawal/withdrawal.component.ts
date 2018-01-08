49d24579af19394e250a96488f2101e169ae71b7
GAM <gam230596@gmail.com>
2018-01-08 23:14:41 +0700

    + tạo giao dịch gửi tiền lên blockchain

--- src/app/Withdrawal/withdrawal.component.ts
+++ src/app/Withdrawal/withdrawal.component.ts
@@ -7,9 +7,36 @@ import { Router, ActivatedRoute, Params } from '@angular/router';
     styleUrls: ['./withdrawal.component.css']
 })
 export class WithdrawalComponent {
-    constructor(public nav: NavService,private route: Router){}
+    constructor(public nav: NavService,private route: Router){
+    	this.status="Khởi tạo";
+    }
+    actual=0;
+    available=0;
+    Address:any;
+    status="Khởi tạo";
+    messageErr;
+    transaction:any=[];
     ngOnInit(){
-    	
+        console.log(this.nav.address);
+    	this.nav.getBalance(this.nav.address).subscribe(balance=>{this.actual=balance;this.available=balance});
+       	setTimeout(()=>{console.log(this.actual);},4000);
+        this.nav.getTransaction(this.nav.address).subscribe(trans=>{this.transaction=trans});
+        setTimeout(()=>{console.log(this.transaction);},4000);
+        /*this.nav.updateActualBalance(this.nav.id,this.actual).subscribe(
+                data=>console.log(data),
+                error=>console.log(error));
+        this.nav.updateAvailableBalance(this.nav.id,this.available).subscribe(
+                data=>console.log(data),
+                error=>console.log(error));*/
+        
+    }
+    Send(address,money){
+    	this.Address=address;
+    	this.nav.transactionsHis(this.nav.address,address,this.status).subscribe(
+            data=>console.log(data),
+            error=>console.log(error),
+            ()=>this.messageErr="Success!");
+
     }
      
 }

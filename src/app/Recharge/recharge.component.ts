36d07a6644b2ed36847d55ed80308e3da721a57c
Tai Chau <tai.chaule@gmail.com>
2018-01-10 11:21:05 +0700

    update recharge

--- src/app/Recharge/recharge.component.ts
+++ src/app/Recharge/recharge.component.ts
@@ -7,13 +7,67 @@ import { Router, ActivatedRoute, Params } from '@angular/router';
     styleUrls: ['./recharge.component.css']
 })
 export class RechargeComponent {
-    constructor(public nav: NavService,private route: Router){}
-    address:any=[];
-    ngOnInit(){
-    	this.nav.hide();
-        let that=this;
-        this.nav.getUser().subscribe(data=>{that.address = data[0].address});
-        setTimeout(()=>{console.log(this.address);},5000);
+    constructor(public nav: NavService,private route: Router){
+        setInterval(()=>{
+            this.nav.getUser().subscribe(data=>{
+                for(var i=0;i<data.length;i++)
+                {
+                    if(this.nav.address===data[i].address)
+                    {
+                        this.actual=data[i].actualbalance;
+                        this.available=data[i].availablebalance;
+                    }
+                }
+                 
+            });
+            this.nav.getBalance(this.nav.address).subscribe(res=>{
+                var balance=parseInt(""+res);
+                if(balance>this.actual){
+                    this.haveDepositors = true;
+                    console.log(balance);
+                    console.log(this.actual);
+                    this.deposits = balance - this.actual;
+                    console.log(this.deposits);
+
+                    this.nav.updateActualBalance(this.nav.id,balance).subscribe(
+                        data=>console.log(data),
+                        err=>console.log(err));
+                    this.nav.updateAvailableBalance(this.nav.id,balance).subscribe(
+                        data=>console.log(data),
+                        err=>console.log(err));
+
+                }
+                else
+                {
+                    this.haveDepositors = false;
+                }
+            });
+        },5000);
     }
-     
+    address:any=[];
+    actual=0;
+    available=0;
+    deposits=0;
+    haveDepositors = false;
+    transaction:any=[];
+
+    ngOnInit(){
+        this.nav.hide();
+        let that=this;
+        this.address = this.nav.address;
+        setTimeout(()=>{console.log(this.address);},5000);
+        //console.log(this.deposits);
+        this.nav.getUser().subscribe(data=>{
+            for(var i=0;i<data.length;i++)
+            {
+                if(this.nav.address===data[i].address)
+                {
+                    this.actual=data[i].actualbalance;
+                    this.available=data[i].availablebalance;
+                }
+            }
+             
+        });
+    }
+      
 }
